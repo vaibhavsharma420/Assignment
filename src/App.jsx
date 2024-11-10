9b68b533f7b80353b2e005852709408f77d36249
Vaibhav Sharma <vaibhavsharma@Vaibhavs-MacBook-Air.local>
2024-11-10 23:29:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,290 @@
+import React from 'react';
+import './index.css';
+
+
+const App = () => {
+  return (
+    <div className="image-row">
+
+      <div className="image-container">
+        <img src="img1aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#e5243b", backgroundImage:"./img1.png" }}>
+          <img src="./img1.png" />
+          <div className="more-text">
+           
+            <p>"End Poverty in all its forms everywhere"</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Poverty Awareness program</li>
+              <li>Demonstration of Saving & Investment</li>
+              <li>Skill Development & Capacity Building</li>
+              <li>Free Food for Children</li>
+              <li>Free Medical Assistance</li>
+              <li>Teri Dava Meri Dava Scheme</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img2aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#dda63a" }}>
+          <img src="./img2.png" />
+          <div className="more-text">
+           
+            <p>“End hunger, achieve food security & improved nutrition & promote
+            sustainable agriculture.”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Food/ ration distribution</li>
+              <li>Community (farmers) Promotion</li>
+              <li>Conducted interactive sessions on oral hygiene practices</li>
+              <li>Performed comprehensive dental examinations</li>
+              <li>Free Food for Patients</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img3aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#4c9f38" }}>
+          <img src="./img3.png" />
+          <div className="more-text">
+            
+            <p>“Ensure healthy lives and promote well-being for all at all ages”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Free Health Camp Check-up</li>
+              <li>Awareness and Prevention of Diseases</li>
+              <li>Free Delivery Scheme for Women</li>
+              <li>Mindfulness, Meditation & Yoga</li>
+              <li>‘Good Touch Bad Touch’ Awareness Program</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img4aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#c5192d" }}>
+          <img src="./img4.png" />
+          <div className="more-text">
+        
+            <p>“Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Equitable quality education</li>
+              <li>Career Counselling Workshops</li>
+              <li>Free education</li>
+              <li>Vidhwa Vidya Daan (Gift of Education for Widows)</li>
+              <li>Personality Development Sessions</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img5aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#ff3a21" }}>
+          <img src="./img5.png" />
+          <div className="more-text">
+    
+            <p>“Achieve gender equality and empower all women and girls”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Free Education for girl child</li>
+              <li>Vidhwa Vidya Daan (Gift of Education for Widows)</li>
+              <li>Strategies to Tackle Unconscious Gender Bias</li>
+              <li>Awareness for Ground-level Workplace Gender Equality</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img6aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#26bde2" }}>
+          <img src="./img6.png" />
+          <div className="more-text">
+            
+            <p>“Ensure availability and sustainable management of water and sanitation for all”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Elimination of Plastic Cups & - Introduction of Eco-Friendly Cups</li>
+              <li>Water Harvesting</li>
+              <li>Biowaste Management & STPs</li>
+              <li>Wastewater Treatment and Reuse</li>
+              <li>Policy Proposals to Identify Challenges</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img7aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#fcc30b" }}>
+          <img src="./img7.png" />
+          <div className="more-text">
+           
+            <p>“Ensure access to affordable, reliable, sustainable and modern energy for all”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Gifting Solar Lamp</li>
+              <li>Awareness programs on renewable energy sources</li>
+              <li>Implementation of solar & biogas energy.</li>
+              <li>Tips to Save Energy & Reduce Consumption</li>
+              <li>Tree planting and Environmental Sustainability</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img9aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#fd6925" }}>
+          <img src="./img9.png" />
+          <div className="more-text">
+           
+            <p>"Build resilient infrastructure, promote inclusive & sustainable industrialization & foster innovation"</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Application of Renewable Energy Sources</li>
+              <li>Industrial Visits & Participation</li>
+              <li>Webinars to Explore Modern Medical Technology</li>
+              <li>Introduction to latest Equipment and technology at Hospital</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img13aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#3f7e44" }}>
+          <img src="./img13.png" />
+          <div className="more-text">
+           
+            <p>“Take urgent action to combat climate change and its impacts”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Plantation Drive</li>
+              <li>Protection of the Aravalli range by ‘Adopt a Hill’ Program</li>
+              <li>Protection of the Aravalli range by ‘Adopt a Hill’ Program</li>
+              <li>Promotion of Sustainable Farming Methods</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src="img15aa.png" alt="Sample" className="image" />
+        <div className="overlay" style={{ backgroundColor: "#56c02b" }}>
+          <img src="./img15.png" />
+          <div className="more-text">
+          
+            <p>“Promote sustainable ecosystems, manage forests, combat degradation, and protect biodiversity”</p>
+            <hr />
+            <p>NIMS Initiatives</p>
+            <ul>
+              <li>Adopt a Hill</li>
+              <li>Plantation Drive</li>
+              <li>Awareness of Conversation of Flora & Fauna</li>
+              <li>Land Degradation Prevention</li>
+              <li>Community Engagement for Sustainable Agriculture</li>
+            </ul>
+            <div className="d-flex justify-content-center">
+              <button
+                type="button"
+                className="btn btn-outline-light d-flex align-items-center justify-content-center"
+              >
+                Read More
+              </button>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
